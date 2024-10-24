b3d87525e829d78934d384279374c696a67e23f3
NelsonDanielFreitas <nelsonfreitas@ipvc.pt>
2024-10-24 12:00:09 +0100

    Teste

--- server.js
+++ server.js
@@ -6,11 +6,9 @@ const port = 3000;
 app.use(cors()); // Habilitar CORS para todas as rotas
 
 const betRoutes = require("./routes/betRoutes");
-const myFickr = require("./routes/myFickr");
 
 // Usar as rotas importadas no caminho "/"
 app.use("/bets", betRoutes);
-app.use("/myFickr", myFickr);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
